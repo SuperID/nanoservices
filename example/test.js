de465e7a7a00022dafb82be9855f7fc3653f100e
Zongmin Lei <leizongmin@gmail.com>
2016-07-15 13:43:19 +0800

    example

--- example/test.js
+++ example/test.js
@@ -2,6 +2,12 @@
 
 const {globalManager, register, call} = require('../');
 
+register('divide', function (ctx) {
+  const a = Number(ctx.params.a);
+  const b = Number(ctx.params.b);
+  ctx.result(a / b);
+});
+
 register('add', function (ctx) {
   if (isNaN(ctx.params.a)) return ctx.error('参数a不是一个数值');
   if (isNaN(ctx.params.b)) return ctx.error('参数a不是一个数值');
@@ -14,12 +20,23 @@ register('add', function (ctx) {
   ctx.result(a + b);
 });
 
-call('add', {a: 123, b: 456}, (err, ret) => {
+register('hello', function (ctx) {
+
+  ctx.call('add', {a: 123, b: 456}, (err, ret1) => {
+    if (err) return ctx.error(err);
+
+    ctx.call('divide', {a: 123, b: 456}, (err, ret2) => {
+      if (err) return ctx.error(err);
+
+      ctx.result({a: ret1, b: ret2});
+    });
+  })
+});
+
+call('hello', {}, (err, ret) => {
   if (err) {
     console.error(err);
   } else {
-    console.log('result=%s', ret);
+    console.log('result=%j', ret);
   }
 });
-
-console.log(globalManager.getService('add'));
