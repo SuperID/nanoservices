aeed1d49a359092b4d0659402b1c83b4bf40908a
Zongmin Lei <leizongmin@gmail.com>
2016-08-17 13:58:54 +0800

    feat: service handler 支持 async function

---
+++ test/test_error.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * super-microservices test
+ *
+ * @author Zongmin Lei <leizongmin@gmail.com>
+ */
+
+const assert = require('assert');
+const Manager = require('../lib/manager');
+
+describe('Service', function () {
+
+  describe('function', function () {
+
+    it('在 service 内出错被捕捉到', function (done) {
+      const manager = new Manager();
+      manager.register('test', function (ctx) {
+        if (Date.now() > 0) {
+          throw new Error('test');
+        }
+        ctx.result({ ok: true });
+      });
+      manager.call('test', {}, (err, _ret) => {
+        assert.equal(err && err.message, 'test');
+        done();
+      });
+    });
+
+  });
+
+  describe('async function (Promise)', function () {
+
+    it('在 service 内出错被捕捉到', function (done) {
+      const manager = new Manager();
+      manager.register('test', function (ctx) {
+        return new Promise((_resolve, _reject) => {
+          if (Date.now() > 0) {
+            throw new Error('test');
+          }
+          ctx.result({ ok: true });
+        });
+      });
+      manager.call('test', {}, (err, _ret) => {
+        assert.equal(err && err.message, 'test');
+        done();
+      });
+    });
+
+  });
+
+});
